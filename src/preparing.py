4f618639b50facdee4709f09cc556df9954c336d
RadishJin <radishj24@gmail.com>
2026-09-13 13:35:15 +0900

    Feat: Refactor decoy generation in preparing.py to support multiple IDs and enhance clarity

--- src/preparing.py
+++ src/preparing.py
@@ -18,25 +18,26 @@ for id in id_list:
 
 
 
-# 각 target의 특징에 따라 torsion angle 기반 decoy 생성
-
-
+# 각 target의 특징에 따라 decoy 생성
+id_single_chain = ["1CRN", "1CLL"]
+decoy_dict = {}
 # 1. 1CRN Crambin (가장 기본형)
 
 
 # (1) Gaussian Coordiante Noise (sigma = 0.5, 1.0, 2.0, 5.0 angstrom)
 
-bb_coord = struc.coord(data_dict["1CRN"])
-bb_tensor = torch.tensor(bb_coord)
-sigmas = [0.5, 1.0, 2.0, 5.0]
-decoy_dict = {}
-for sigma in sigmas:
-    noise = torch.randn_like(bb_tensor) * sigma
-    noise_coord = bb_tensor + noise
-    noise_coord = noise_coord.detach().cpu().numpy()
-    pre1 = data_dict["1CRN"].copy()
-    struc.coord(pre1)[:] = noise_coord
-    decoy_dict[f"1CRN_{sigma}angstrom"] = pre1
+# single chain
+for id in id_single_chain:
+    bb_coord = struc.coord(data_dict[f"{id}"])
+    bb_tensor = torch.tensor(bb_coord)
+    sigmas = [0.5, 1.0, 2.0, 5.0]
+    for sigma in sigmas:
+        noise = torch.randn_like(bb_tensor) * sigma
+        noise_coord = bb_tensor + noise
+        noise_coord = noise_coord.detach().cpu().numpy()
+        pre1 = data_dict[f"{id}"].copy()
+        struc.coord(pre1)[:] = noise_coord
+        decoy_dict[f"{id}_{sigma}angstrom"] = pre1
     
     
 # test
@@ -47,54 +48,56 @@ for sigma in sigmas:
 
 # (2) Global Perturbation (Gaussian, sigma = 5, 10 degree)
 
-# 바꿀 토션앵글 값 구하기
-bb_phi, bb_psi, bb_omega = struc.dihedral_backbone(data_dict["1CRN"])
-bb_phi_tensor = torch.tensor(bb_phi)
-bb_psi_tensor = torch.tensor(bb_psi)
-bb_omega_tensor = torch.tensor(bb_omega)
-tensor_list = [bb_phi_tensor, bb_psi_tensor, bb_omega_tensor]
-angle_noise_list = [pi/36, pi/18]
-pre_decoy = []
-for angle in angle_noise_list:
-    for tensor in tensor_list:
-        noise = torch.randn_like(tensor) * angle
-        noise = noise.detach().cpu().numpy().tolist()
-        pre_decoy.append(noise[0])
+# single chain
+for id in id_single_chain:
+    # 바꿀 토션앵글 값 구하기
+    bb_phi, bb_psi, bb_omega = struc.dihedral_backbone(data_dict[f"{id}"])
+    bb_phi_tensor = torch.tensor(bb_phi)
+    bb_psi_tensor = torch.tensor(bb_psi)
+    bb_omega_tensor = torch.tensor(bb_omega)
+    tensor_list = [bb_phi_tensor, bb_psi_tensor, bb_omega_tensor]
+    angle_noise_list = [pi/36, pi/18]
+    pre_decoy = []
+    for angle in angle_noise_list:
+        for tensor in tensor_list:
+            noise = torch.randn_like(tensor) * angle
+            noise = noise.detach().cpu().numpy().tolist()
+            pre_decoy.append(noise[0])
 
-# print(bb_omega)
-# print(pre_decoy[5])
+    # print(bb_omega)
+    # print(pre_decoy[5])
 
-# 델타 토션앵글 리스트화
-five_degree = [i for j in zip(pre_decoy[0], pre_decoy[1], pre_decoy[2]) for i in j]
-ten_degree = [i for j in zip(pre_decoy[3], pre_decoy[4], pre_decoy[5]) for i in j]
-# print(ten_degree)
+    # 델타 토션앵글 리스트화
+    five_degree = [i for j in zip(pre_decoy[0], pre_decoy[1], pre_decoy[2]) for i in j]
+    ten_degree = [i for j in zip(pre_decoy[3], pre_decoy[4], pre_decoy[5]) for i in j]
+    # print(ten_degree)
 
-# 처음과 끝 nan으로 바꾸기
-five_degree = five_degree[1:-1]
-ten_degree = ten_degree[1:-1]
-# print(five_degree, ten_degree)
+    # 쓰이지 않는, 의미없는 값 잘라내기
+    five_degree = five_degree[1:-1]
+    ten_degree = ten_degree[1:-1]
+    # print(five_degree, ten_degree)
 
-pre2 = data_dict["1CRN"][0].copy()
-# print(pre[0].coord)
 
-# N-term부터 하나씩 돌리면서 새 구조 만들기
-k = 0
-for angle in five_degree, ten_degree:
-    for i in range(len(angle)):
-        if i+2 > len(pre2):
-            break
-        axis = pre2[i+1].coord - pre2[i].coord
-        support = pre2[i+1].coord
-        downstream = pre2[i+2:]
-        downstream = struc.rotate_about_axis(
-            downstream,
-            angle = angle[i],
-            axis = axis,
-            support = support
-        )
-        struc.coord(pre2[i+2:])[:] = struc.coord(downstream)
-    k += 5
-    decoy_dict[f"1CRN_{k}degree"] = pre2
+    # print(pre[0].coord)
+    # N-term부터 하나씩 돌리면서 새 구조 만들기
+    j = 0
+    for angle in five_degree, ten_degree:
+        pre2 = data_dict[f"{id}"][0].copy()
+        for i in range(len(angle)):
+            if i+2 > len(pre2):
+                break
+            axis = pre2[i+1].coord - pre2[i].coord
+            support = pre2[i+1].coord
+            downstream = pre2[i+2:]
+            downstream = struc.rotate_about_axis(
+                downstream,
+                angle = angle[i],
+                axis = axis,
+                support = support
+            )
+            struc.coord(pre2[i+2:])[:] = struc.coord(downstream)
+        j += 5
+        decoy_dict[f"{id}_{j}degree"] = pre2
 
 
 # test
@@ -103,16 +106,54 @@ for angle in five_degree, ten_degree:
 # print(five_degree)
 # print(pre2)
 
+# print(decoy_dict.keys())
+
+
+# (3) Local Extreme Perturbation (대충 가운데 부근 한 곳에서 결합각 대폭 조정, 30도 60도.)
+
+# single chain
+for id in id_single_chain:
+    # 원본 데이터셋 가져와서
+    pre3 = data_dict[f"{id}"][0].copy()
+    pre4 = data_dict[f"{id}"][0].copy()
+
+    # 돌릴 각도 저장해놓고
+    torsion = [pi/6, pi/3]
+
+    # 대충 중간 지점 정하고
+    mid = int(len(pre3)/2)
+    # print(mid)
+
+    # 변형 - 30degree
+    downstream = pre3[mid:]
+    axis = pre3[mid - 1].coord - pre3[mid - 2].coord
+    support = pre3[mid-1].coord
+    downstream = struc.rotate_about_axis(
+        downstream,
+        angle = torsion[0],
+        axis = axis,
+        support = support
+    )
+    struc.coord(pre3[mid:])[:] = struc.coord(downstream)
+    decoy_dict[f"{id}_ex30"] = pre3
+
+    # 변형 - 60degree
+    downstream = pre4[mid:]
+    axis = pre4[mid-1].coord - pre4[mid-2].coord
+    support = pre4[mid-1].coord
+    downstream = struc.rotate_about_axis(
+        downstream,
+        angle = torsion[1],
+        axis = axis,
+        support = support
+    )
+    struc.coord(pre4[mid:])[:] = struc.coord(downstream)
+    decoy_dict[f"{id}_ex60"] = pre4
+
+
 print(decoy_dict.keys())
-
-
-
-
-
-
-# (3) Local Extreme Perturbation
-
-
+# print(decoy_dict["1CRN_ex60"][60:80])
+# print(data_dict["1CRN"][0][60:80])
 
 
 
